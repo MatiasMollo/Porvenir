8b8856b8671e58ef2f3fd4030483dcbadf1af63c
Francisco <frandex58@gmail.com>
2022-10-11 16:20:30 -0300

    actualizacion de index.php

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <?php
-include "addons/nav.php";
 if(isset($_SESSION['user'])) var_dump($_SESSION['user']);
 ?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/templateStyles.css">
     <title>Inicio - El Porvenir</title>
   </head>
   <body>
+    <?php include("addons/navGuest.php");?>
+    <main>
+        <div>
+          
+        </div>
+    </main>
+    <?php 
+      require_once("addons/sidebarLoginGuest.php");
+      require_once("addons/footer.php");
+    ?>
 
   </body>
 </html>
