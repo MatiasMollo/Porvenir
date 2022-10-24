e148183e7a9badc7f8bf0e1393a6c9f84f6ff4e5
Francisco <frandex58@gmail.com>
2022-10-24 13:37:22 -0300

    responsive navbar

--- addons/javaScript/sidebarScript.js
+++ addons/javaScript/sidebarScript.js
@@ -1,19 +1,24 @@
 var openLogin=document.querySelectorAll('.openSidebarLogin');
 var closeLogin=document.querySelectorAll('.closeSidebarLogin');
 var sidebarLogin=document.querySelector('.sidebar--login');
-
+var barMenu=document.querySelectorAll('.nav__bar');
 window.addEventListener('load',loadEvents=_=>{
     openLogin.forEach(e=>{e.addEventListener('click',printLogin)});
     closeLogin.forEach(e=>{e.addEventListener('click',printLogin)});
 });
 
 function printLogin() { 
-    if(sidebarLogin.style.display=='flex') {sidebarLogin.style.display='none'; iconLogin();}
-    else {sidebarLogin.style.display='flex'; iconLogin();}
+    if(sidebarLogin.style.display=='flex') {sidebarLogin.style.display='none'; iconLogin(); if(screen.width<1023) barMenu.forEach(e=>e.style.borderRadius="15px 15px 0px 0px")}
+    else {sidebarLogin.style.display='flex'; iconLogin(); }
 };
 function iconLogin() {
     openLogin.forEach(e=>{if(e.style.display=='none') e.style.display='flex'; else e.style.display='none'});
     closeLogin.forEach(e=>{if(e.style.display=='flex') e.style.display='none'; else e.style.display='flex'});
+   if(screen.width<1023)  barMenu.forEach(e=>e.style.borderRadius="15px 0px 0px 0px")
 };
 
-function closeAllLogin() {if(sidebarLogin.style.display=='flex') printLogin()};
+function closeAllLogin() {
+    if(sidebarLogin.style.display=='flex') printLogin()
+    
+};
+function closeAllSidebar(){closeAllLogin()};
--- addons/navGuest.php
+++ addons/navGuest.php
@@ -8,29 +8,27 @@
             <img class="nav__icon nav__icon--right closeSidebarLogin" src="addons/images/navImages/iconCloseSidebar.svg" alt="Boton Usuario">
 
         </div>
-
+        <img class="nav__logo nav__logo--mobile nav__logo--open" src="addons/images/navImages/logoPorvenir.png" alt="Porvenir Logo" >
+        <svg class="nav__logo nav__logo--mobile nav__logo--close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+            <path fill-rule="evenodd" d="M8.41421356,7 L10.5355339,4.87867966 C10.9260582,4.48815536 10.9260582,3.85499039 10.5355339,3.46446609 C10.1450096,3.0739418 9.51184464,3.0739418 9.12132034,3.46446609 L7,5.58578644 L4.87867966,3.46446609 C4.48815536,3.0739418 3.85499039,3.0739418 3.46446609,3.46446609 C3.0739418,3.85499039 3.0739418,4.48815536 3.46446609,4.87867966 L5.58578644,7 L3.46446609,9.12132034 C3.0739418,9.51184464 3.0739418,10.1450096 3.46446609,10.5355339 C3.85499039,10.9260582 4.48815536,10.9260582 4.87867966,10.5355339 L7,8.41421356 L9.12132034,10.5355339 C9.51184464,10.9260582 10.1450096,10.9260582 10.5355339,10.5355339 C10.9260582,10.1450096 10.9260582,9.51184464 10.5355339,9.12132034 L8.41421356,7 Z"/>
+        </svg>
         <ul class="nav__list">
             <a href="invitado.php" class="nav__btn">
                 <div class="nav__btnBox">
                     Principal
                 </div>
             </a>
-
             <a href="" class="nav__btn">
                 <div class="nav__btnBox">
                     Noticias
                 </div>
             </a>
-
-            
-            <img src="addons/images/navImages/logoPorvenir.png" alt="Porvenir Logo" class="nav__logoPorvenir">
-
+            <img class="nav__logo nav__logo--desktop" src="addons/images/navImages/logoPorvenir.png" alt="Porvenir Logo" >
             <a href="" class="nav__btn">
                 <div class="nav__btnBox">
                     Torneos
                 </div>
             </a>
-
             <a href="" class="nav__btn">
                 <div class="nav__btnBox">
                     Contacto
@@ -40,3 +38,4 @@
     </nav>    
 </div>
 <link rel="stylesheet" href="addons/css/navStyles.css">
+<script src="addons/javaScript/navScript.js"></script>
